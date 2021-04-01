f0236fefcb0cfd0e8778db757aad61d163c4693f
Antony Leme <antony040402@gmail.com>
2021-04-01 13:02:32 -0300

    remove dates comments

--- src/services/dates.js
+++ src/services/dates.js
@@ -2,18 +2,11 @@ function month() {
   var today = new Date()
   var priorDate = new Date().setDate(today.getDate() - 30)
   var firstDay = new Date(priorDate)
-  // let date = new Date(), y = date.getFullYear(), m = date.getMonth();
-  // let firstDay = new Date(y, m, 1);
-  // let lastDay = new Date(y, m + 1, 0);
 
   return { firstDay: firstDay.toISOString().split('T')[0], lastDay: today.toISOString().split('T')[0] }
 }
 
 function week() {
-  // let curr = new Date; // get current date
-  // let first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
-  // let last = first + 6; // last day is the first day + 6
-
   var today = new Date()
   var priorDate = new Date().setDate(today.getDate() - 7)
   var firstDay = new Date(priorDate)
